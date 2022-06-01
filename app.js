007d44dc10f7a95c40de56e2494cdcbb27c3a836
PawelPalasinski <pawel.palasinski@gmail.com>
2022-06-02 00:16:28 +0200

    images

---
+++ app.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardArr = [
+    {
+      name: "black",
+      img: "images/black.png",
+    },
+    {
+      name: "blue",
+      img: "images/blue.png",
+    },
+    {
+      name: "dc",
+      img: "images/dc.png",
+    },
+    {
+      name: "green",
+      img: "images/green.png",
+    },
+    {
+      name: "indigo",
+      img: "images/indigo.png",
+    },
+    {
+      name: "orange",
+      img: "images/orange.png",
+    },
+    {
+      name: "red",
+      img: "images/red.png",
+    },
+    {
+      name: "saphire",
+      img: "images/saphire.png",
+    },
+    {
+      name: "white",
+      img: "images/white.png",
+    },
+    {
+      name: "yellow",
+      img: "images/yellow.png",
+    }
+    ]
+    
+    const grid = document.querySelector('.grid')
+
+    function createBoard() {
+        for (let i = 0; i < cardArr.length; i++) {
+            var card
+        }
+    }
+});
